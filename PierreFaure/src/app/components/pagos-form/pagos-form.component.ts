0ae1910a96f18ec659edee8372ad7221a0a447c2
Dario Raul Cruz Ramirez <dariocruz100@gmail.com>
2021-05-17 19:44:09 -0500

    Pagos y login

--- PierreFaure/src/app/components/pagos-form/pagos-form.component.ts
+++ PierreFaure/src/app/components/pagos-form/pagos-form.component.ts
@@ -1,6 +1,8 @@
-import { Component, Injectable, OnInit } from '@angular/core';
+import { Component, ElementRef, Injectable, OnInit, ViewChild } from '@angular/core';
+import { async } from '@angular/core/testing';
 import { Router } from '@angular/router';
 
+//declare var paypal;
 @Injectable()
 @Component({
   selector: 'app-pagos-form',
@@ -9,12 +11,103 @@ import { Router } from '@angular/router';
 })
 export class PagosFormComponent implements OnInit {
 
+  //@ViewChild('paypal', {static:true}) paypalElement:ElementRef;
+
+  pagos = {
+    descripcion: 'Pago Pierre Faure',
+    precio: '1500',
+
+  }
+
   constructor(private router:Router) { }
 
-  ngOnInit(): void {
+  ngOnInit(){}
+
+  /*ngOnInit(): void {
+    paypal.buttons({
+      createOrder:(data,actions) =>{
+        return actions.order.create({
+          purchase_units:[{
+            description: this.pagos.descripcion,
+            amount:{
+              currency_code: 'MXM',
+              value: this.pagos.precio
+            }
+          }]
+        })
+      },
+      onApprove: async(data,actions)=>{
+        const order = await actions.order.capture();
+        console.log(order)
+      },
+      onError: err =>{
+        console.log(err);
+      }
+    }).render(this.paypalElement.nativeElement);
   }
+  */
 
   cancelar() {
     this.router.navigate(['admin']);
   }
+
+
+
+
+
+
+
+
+  
+  /*paymentRequest : google.payments.api.PaymentDataRequest = {
+    apiVersion: 2,
+    apiVersionMinor: 0,
+    allowedPaymentMethods: [
+      {
+        type: 'CARD',
+        parameters: {
+          allowedAuthMethods: ['PAN_ONLY', 'CRYPTOGRAM_3DS'],
+          allowedCardNetworks: ['VISA', 'MASTERCARD', 'AMEX'],
+        },
+        tokenizationSpecification: {
+          type: 'PAYMENT_GATEWAY',
+          parameters: {
+            gateway: 'example',
+            gatewayMerchantId: 'examplegatewayMerchantId',
+
+          },
+        },
+      },
+    ],
+  merchantInfo: {
+    merchantId: '12345678901234567890',
+    merchantName: 'Colegio Pierre Faure',
+  },
+  transactionInfo: {
+    totalPriceStatus: 'FINAL',
+    totalPriceLabel: 'Total',
+    totalPrice: '1500.00',
+    currencyCode:  'MXN',
+    countryCode: 'MX'
+  },
+  callbackIntents:  ['PAYMENT_AUTHORIZATION']
+};
+
+onLoadPaymentData = (
+  event : Event): void => {
+    const eventDetail = event as CustomEvent<google.payments.api.PaymentData>;
+    console.log('load payment data', eventDetail.detail);
+  }
+
+onPaymentDataAuthorized: google.payments.api.PaymentAuthorizedHandler = (paymentData) => {
+  console.log('payment authorized', paymentData);
+  return {
+    transactionState: 'SUCCESS'
+  };
+}
+
+onError = (event: ErrorEvent): void => {
+  console.error('error', event.error);
+}
+*/
 }
